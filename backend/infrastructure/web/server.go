a92415b9155ecbdb8f634151b3d47a6af7b79309
tocoteron <tocoteron@gmail.com>
2022-07-31 20:19:52 +0900

    Refactor

---
+++ backend/infrastructure/web/server.go
@@ -0,0 +1,95 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/tocoteron/kankaku/graph"
+	"github.com/tocoteron/kankaku/graph/generated"
+	"github.com/tocoteron/kankaku/infrastructure/web/auth"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type server struct {
+	config serverConfig
+}
+
+type serverConfig struct {
+	port   string
+	secret []byte
+}
+
+func NewServer(port string, secret []byte) *server {
+	return &server{
+		config: serverConfig{
+			port:   port,
+			secret: secret,
+		},
+	}
+}
+
+func (s *server) Run() {
+	// Create server
+	e := echo.New()
+
+	// Set middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Set auth routes
+	e.POST("/signup", signup(s.config.secret))
+
+	// Set GraphQL routes
+	e.POST("/graphql", graphqlHandler(), auth.TokenValidator(s.config.secret), auth.UserContextProvider())
+	e.GET("/playground", graphqlPlaygroundHandler("/graphql"))
+
+	// Start server
+	serverPort := fmt.Sprintf(":%s", s.config.port)
+	e.Logger.Fatal(e.Start(serverPort))
+}
+
+// GraphQL
+func graphqlHandler() echo.HandlerFunc {
+	h := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: &graph.Resolver{}},
+		),
+	)
+	return echo.WrapHandler(h)
+}
+
+// GraphQL playground
+func graphqlPlaygroundHandler(graphqlPath string) echo.HandlerFunc {
+	h := playground.Handler("GraphQL playground", graphqlPath)
+	return echo.WrapHandler(h)
+}
+
+func signup(secret []byte) echo.HandlerFunc {
+	type Req struct {
+		ID       string `json:"id"`
+		Password string `json:"password"`
+	}
+
+	type Res struct {
+		Token string `json:"token"`
+	}
+
+	return func(c echo.Context) error {
+		req := &Req{}
+
+		if err := c.Bind(req); err != nil {
+			return err
+		}
+
+		token, err := auth.GenerateToken(0, secret)
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, Res{Token: token})
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/tocoteron/kankaku/auth"
-	"github.com/tocoteron/kankaku/graph"
-	"github.com/tocoteron/kankaku/graph/generated"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/tocoteron/kankaku/infrastructure/web"
 )
 
 // Get environment var or default value
@@ -36,63 +28,7 @@ func main() {
 	port := getEnvOrDefault("PORT", "8080")
 	secret := []byte(getEnvOrPanic("SECRET"))
 
-	// Create server
-	e := echo.New()
-
-	// Set middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Set auth routes
-	e.POST("/signup", signup(secret))
-
-	// Set GraphQL routes
-	e.POST("/graphql", graphqlHandler(), auth.TokenValidator(secret), auth.UserContextProvider())
-	e.GET("/playground", graphqlPlaygroundHandler("/graphql"))
-
 	// Start server
-	serverPort := fmt.Sprintf(":%s", port)
-	e.Logger.Fatal(e.Start(serverPort))
-}
-
-// GraphQL
-func graphqlHandler() echo.HandlerFunc {
-	h := handler.NewDefaultServer(
-		generated.NewExecutableSchema(
-			generated.Config{Resolvers: &graph.Resolver{}},
-		),
-	)
-	return echo.WrapHandler(h)
-}
-
-// GraphQL playground
-func graphqlPlaygroundHandler(graphqlPath string) echo.HandlerFunc {
-	h := playground.Handler("GraphQL playground", graphqlPath)
-	return echo.WrapHandler(h)
-}
-
-func signup(secret []byte) echo.HandlerFunc {
-	type Req struct {
-		ID       string `json:"id"`
-		Password string `json:"password"`
-	}
-
-	type Res struct {
-		Token string `json:"token"`
-	}
-
-	return func(c echo.Context) error {
-		req := &Req{}
-
-		if err := c.Bind(req); err != nil {
-			return err
-		}
-
-		token, err := auth.GenerateToken(0, secret)
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, Res{Token: token})
-	}
+	server := web.NewServer(port, secret)
+	server.Run()
 }
