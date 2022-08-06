44f1982b0b5fe168f18b6eb8733120dcd4ef859d
tocoteron <tocoteron@gmail.com>
2022-08-06 23:52:27 +0900

    Refactor

--- backend/interface/handler/graphql/model/post.go
+++ backend/interface/handler/graphql/model/post.go
@@ -1,6 +1,15 @@
 package model
 
+import "github.com/tocoteron/kankaku/domain/model/post"
+
 type Post struct {
 	ID      string `json:"id"`
 	Content string `json:"content"`
+}
+
+func PostFrom(p *post.Post) *Post {
+	return &Post{
+		ID:      p.ID().String(),
+		Content: p.Content(),
+	}
 }
--- backend/interface/handler/graphql/model/user.go
+++ backend/interface/handler/graphql/model/user.go
@@ -1,7 +1,22 @@
 package model
 
+import "github.com/tocoteron/kankaku/domain/model/user"
+
 type User struct {
 	ID    string  `json:"id"`
 	Name  string  `json:"name"`
 	Posts []*Post `json:"posts"`
+}
+
+func UserFrom(u *user.User) *User {
+	ps := []*Post{}
+	for _, p := range u.Posts() {
+		ps = append(ps, PostFrom(&p))
+	}
+
+	return &User{
+		ID:    u.ID().String(),
+		Name:  u.Name(),
+		Posts: ps,
+	}
 }
