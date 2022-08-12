f06d3d0554a3b5e0f3750537c2bd95cfbbb97c49
tocoteron <tocoteron@gmail.com>
2022-08-12 22:04:44 +0900

    Refactor

--- backend/domain/model/post_test.go
+++ backend/domain/model/post_test.go
@@ -11,39 +11,39 @@ func TestNewPost(t *testing.T) {
 	tooLongContent := test.RandomString(257)
 	maxLenContent := test.RandomString(256)
 
-	tests := []struct {
+	tests := map[string]struct {
 		id       PostID
 		authorID UserID
 		content  string
 		want     *Post
 	}{
-		{ // Empty content
-			NewPostID("Post0"),
-			NewUserID("User0"),
-			"",
-			nil,
+		"empty content": {
+			id:       NewPostID("Post0"),
+			authorID: NewUserID("User0"),
+			content:  "",
+			want:     nil,
 		},
-		{ // Too long content
-			NewPostID("Post1"),
-			NewUserID("User1"),
-			tooLongContent,
-			nil,
+		"too long content": {
+			id:       NewPostID("Post1"),
+			authorID: NewUserID("User1"),
+			content:  tooLongContent,
+			want:     nil,
 		},
-		{ // Max length content
-			NewPostID("Post2"),
-			NewUserID("User2"),
-			maxLenContent,
-			&Post{
+		"max length content": {
+			id:       NewPostID("Post2"),
+			authorID: NewUserID("User2"),
+			content:  maxLenContent,
+			want: &Post{
 				id:       NewPostID("Post2"),
 				authorID: NewUserID("User2"),
 				content:  maxLenContent,
 			},
 		},
-		{ // Normal content
-			NewPostID("Post3"),
-			NewUserID("User3"),
-			"Hello",
-			&Post{
+		"normal content": {
+			id:       NewPostID("Post3"),
+			authorID: NewUserID("User3"),
+			content:  "Hello",
+			want: &Post{
 				id:       NewPostID("Post3"),
 				authorID: NewUserID("User3"),
 				content:  "Hello",
@@ -51,8 +51,10 @@ func TestNewPost(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		got, _ := NewPost(tt.id, tt.authorID, tt.content)
-		assert.Equal(t, tt.want, got)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := NewPost(tt.id, tt.authorID, tt.content)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
--- backend/interface/handler/graphql/resolver/user.resolvers_test.go
+++ backend/interface/handler/graphql/resolver/user.resolvers_test.go
@@ -26,33 +26,35 @@ func TestMe(t *testing.T) {
 	me, _ := app.UserUseCase().CreateUser("test user")
 	require.NotNil(t, me)
 
-	tests := []struct {
+	tests := map[string]struct {
 		ctx  context.Context
 		want *dto.User
 	}{
-		{ // Can't get user because context is empty
-			context.Background(),
-			nil,
+		"empty context": {
+			ctx:  context.Background(),
+			want: nil,
 		},
-		{ // Can't get user because specified user id is invalid
-			mycontext.SetUserContext(
+		"non-existent user": {
+			ctx: mycontext.SetUserContext(
 				context.Background(),
 				&mycontext.UserContext{},
 			),
-			nil,
+			want: nil,
 		},
-		{ // Can get user because specified user id is valid
-			mycontext.SetUserContext(
+		"me": {
+			ctx: mycontext.SetUserContext(
 				context.Background(),
 				&mycontext.UserContext{ID: me.ID().String()},
 			),
-			dto.UserFrom(me),
+			want: dto.UserFrom(me),
 		},
 	}
 
-	for _, tt := range tests {
-		got, _ := r.Query().Me(tt.ctx)
-		assert.Equal(t, tt.want, got)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := r.Query().Me(tt.ctx)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
 
@@ -62,26 +64,28 @@ func TestUser(t *testing.T) {
 	u, _ := app.UserUseCase().CreateUser("test user")
 	require.NotNil(t, u)
 
-	tests := []struct {
+	tests := map[string]struct {
 		ctx    context.Context
 		userID string
 		want   *dto.User
 	}{
-		{ // Can't get user because specified user id is invalid
+		"non-existent user": {
 			context.Background(),
 			"",
 			nil,
 		},
-		{ // Can get user because specified user id is valid
+		"existing user": {
 			context.Background(),
 			u.ID().String(),
 			dto.UserFrom(u),
 		},
 	}
 
-	for _, tt := range tests {
-		got, _ := r.Query().User(tt.ctx, tt.userID)
-		assert.Equal(t, tt.want, got)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := r.Query().User(tt.ctx, tt.userID)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
 
@@ -94,25 +98,27 @@ func TestPosts(t *testing.T) {
 	p, _ := app.UserUseCase().CreatePost(u.ID(), "test post")
 	require.NotNil(t, p)
 
-	tests := []struct {
+	tests := map[string]struct {
 		ctx  context.Context
 		obj  *dto.User
 		want []*dto.Post
 	}{
-		{ // Can't get posts because specified user id is invalid
-			context.Background(),
-			&dto.User{},
-			nil,
+		"non-existent user": {
+			ctx:  context.Background(),
+			obj:  &dto.User{},
+			want: nil,
 		},
-		{ // Can get posts because specified user id is valid
-			context.Background(),
-			dto.UserFrom(u),
-			dto.PostsFrom(&[]model.Post{*p}),
+		"existing user": {
+			ctx:  context.Background(),
+			obj:  dto.UserFrom(u),
+			want: dto.PostsFrom(&[]model.Post{*p}),
 		},
 	}
 
-	for _, tt := range tests {
-		got, _ := r.User().Posts(tt.ctx, tt.obj)
-		assert.Equal(t, tt.want, got)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := r.User().Posts(tt.ctx, tt.obj)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
