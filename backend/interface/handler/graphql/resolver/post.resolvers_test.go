0b6c4714fceacbf13bf92ea6a36d969c96c3a147
tocoteron <tocoteron@gmail.com>
2022-08-12 23:44:27 +0900

    Create a test for post resolvers

---
+++ backend/interface/handler/graphql/resolver/post.resolvers_test.go
@@ -0,0 +1,63 @@
+package resolver
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tocoteron/kankaku/domain/model"
+	mycontext "github.com/tocoteron/kankaku/interface/handler/context"
+	dto "github.com/tocoteron/kankaku/interface/handler/graphql/model"
+)
+
+func TestPost(t *testing.T) {
+	app, r := setupResolver()
+
+	u, _ := app.UserUseCase().CreateUser("User")
+	require.NotNil(t, u)
+
+	pid, _ := model.NewPostID("Post ID")
+	require.NotNil(t, pid)
+	p, _ := model.NewPost(*pid, u.ID(), "Post")
+	require.NotNil(t, p)
+
+	tests := map[string]struct {
+		ctx     context.Context
+		content string
+		want    *dto.Post
+	}{
+		"empty context": {
+			ctx:     context.Background(),
+			content: "test content",
+			want:    nil,
+		},
+		"non-existent user": {
+			ctx: mycontext.SetUserContext(
+				context.Background(),
+				&mycontext.UserContext{},
+			),
+			content: "test content",
+			want:    nil,
+		},
+		"existing user": {
+			ctx: mycontext.SetUserContext(
+				context.Background(),
+				&mycontext.UserContext{ID: u.ID().String()},
+			),
+			content: p.Content(),
+			want:    dto.PostFrom(p),
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := r.Mutation().Post(tt.ctx, tt.content)
+			if tt.want != nil && got != nil {
+				assert.Equal(t, tt.want.Content, got.Content)
+				assert.Equal(t, tt.want.AuthorID, got.AuthorID)
+			}
+		})
+	}
+}
